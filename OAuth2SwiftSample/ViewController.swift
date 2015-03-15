feac5c7afe98813909d78b604564d4dcd7e59477
加藤 雄大 <takahiro.kato15@gmail.com>
2015-03-15 21:00:35 +0900

    Initial Commit

---
+++ OAuth2SwiftSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OAuth2SwiftSample
+//
+//  Created by 加藤 雄大 on 2015/03/15.
+//  Copyright (c) 2015年 grandbig.github.io. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
